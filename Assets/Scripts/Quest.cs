67107af5e6b56a288823992ffbc9be9aa1e3b94a
Shintarouz <thomas.schaer1@gmail.com>
2025-03-18 11:18:21 +0100

    Sprite Atlas Update

--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -1,91 +1,91 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Quest : ScriptableObject
-{
-    [System.Serializable]
+// public class Quest : ScriptableObject
+// {
+//     [System.Serializable]
 
-    public struct Info
-    {
-        public string Name;
-        public Sprite Icon;
-        public string Description;
-    }
-    [Header("Info")] public Info Information;
+//     public struct Info
+//     {
+//         public string Name;
+//         public Sprite Icon;
+//         public string Description;
+//     }
+//     [Header("Info")] public Info Information;
 
-    [System.Serializable]
+//     [System.Serializable]
 
-    public struct Stat
-    {
-        public int Currency;
-        public int XP;
-    }
+//     public struct Stat
+//     {
+//         public int Currency;
+//         public int XP;
+//     }
 
-    [Header("Reward")] public Stat Reward = new stat {Currency = 10, XP = 0};
+//     [Header("Reward")] public Stat Reward = new stat {Currency = 10, XP = 0};
 
-    public bool Completed { get; private set; }
-    public QuestCompletedEvent QuestCompleted;
+//     public bool Completed { get; private set; }
+//     public QuestCompletedEvent QuestCompleted;
 
-    public abstract class QuestGoal : ScriptableObject
-    {
-        protected string Description;
-        public int CurrentAmount { get; protected set; }
-        public int RequiredAmount = 1;
+//     public abstract class QuestGoal : ScriptableObject
+//     {
+//         protected string Description;
+//         public int CurrentAmount { get; protected set; }
+//         public int RequiredAmount = 1;
 
-        public bool Completed { get; protected set; }
-        [HideInInspector] public UnityEvent GoalCompleted;
+//         public bool Completed { get; protected set; }
+//         [HideInInspector] public UnityEvent GoalCompleted;
 
-        public virtual string GetDescription()
-        {
-            return Description;
-        }
+//         public virtual string GetDescription()
+//         {
+//             return Description;
+//         }
 
-        public virtual void Initialize()
-        {
-            Completed = false;
-            GoalCompleted = new UnityEvent();
-        }
+//         public virtual void Initialize()
+//         {
+//             Completed = false;
+//             GoalCompleted = new UnityEvent();
+//         }
 
-        protected void Evaluate()
-        {
-            if ( CurrentAmount >= RequiredAmount )
-            {
-                Complete();
-            }
-        }
-        private void Complete()
-        {
-            Completed = true;
-            GoalCompleted.Invoke();
-            GoalCompleted.RemoveAllListeners();
-        }
+//         protected void Evaluate()
+//         {
+//             if ( CurrentAmount >= RequiredAmount )
+//             {
+//                 Complete();
+//             }
+//         }
+//         private void Complete()
+//         {
+//             Completed = true;
+//             GoalCompleted.Invoke();
+//             GoalCompleted.RemoveAllListeners();
+//         }
 
-        public void Skip()
-        {
-            // charge player currency
-            Complete();
-        }
-    }
+//         public void Skip()
+//         {
+//             // charge player currency
+//             Complete();
+//         }
+//     }
 
-    public List<QuestGoal> Goals;
+//     public List<QuestGoal> Goals;
 
-    public void Initialize()
-    {
-        Completed = false;
-        QuestCompleted = new QuestCompletedEvent();
+//     public void Initialize()
+//     {
+//         Completed = false;
+//         QuestCompleted = new QuestCompletedEvent();
 
-        foreach (var goal in Goals)
-        {
-            goal.Initialize();
-            goal.GoalCompleted.AddListener(delegate { CheckGoals(); });
-        }
-    }
+//         foreach (var goal in Goals)
+//         {
+//             goal.Initialize();
+//             goal.GoalCompleted.AddListener(delegate { CheckGoals(); });
+//         }
+//     }
 
-    private void CheckGoals()
-    {
-        Completed = Goals.All(g => g.Quest)
-    }
-}
+//     private void CheckGoals()
+//     {
+//         Completed = Goals.All(g => g.Quest)
+//     }
+// }
 
-public class QuestCompletedEvent : UnityEvent<Quest> { }
+// public class QuestCompletedEvent : UnityEvent<Quest> { }
